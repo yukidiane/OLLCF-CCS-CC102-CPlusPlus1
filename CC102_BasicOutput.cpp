16c92b20f89bf52b689c429f5578d5775992c6e5
Dayanne Yuki Hatori <danaya16238@gmail.com>
2024-10-18 09:12:25 +0800

    Add files via upload

---
+++ CC102_BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "Bachelor of Science in Information Technology-1st Year" << endl;
+	cout << "Dayanne Yuki R. Hatori" << endl;
+	cout << "Purok-2, Lag-on, Daet, Camarines Norte"<< endl;
+	return 0;
+}
