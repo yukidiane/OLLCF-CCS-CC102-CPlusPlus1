b81d59c461e0093d9af8ada1a8d8b8926a0d935c
Dayanne Yuki Hatori <danaya16238@gmail.com>
2024-10-18 09:15:36 +0800

    Add files via upload

---
+++ CC102_03VariableDeclaration.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age;
+	
+	cout << "Enter you age\t:";
+	cin >> age;
+	cout << "Your age is\t;" << age << endl;
+	cout << "Thank you!" << endl;
+	
+	return 0;
+	
+}
